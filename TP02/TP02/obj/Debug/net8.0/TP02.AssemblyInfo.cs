dfe1d03bcce6b5c2d07ed98c7f4ca3ba0f88d96a
Mahesazain <echa.zain@gmail.com>
2024-03-03 22:44:28 +0800

    Tambahan fitur GUI

--- TP02/TP02/obj/Debug/net8.0/TP02.AssemblyInfo.cs
+++ TP02/TP02/obj/Debug/net8.0/TP02.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d45db5d8190b2dfe4a8788a7a5c7d81200e7573")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
