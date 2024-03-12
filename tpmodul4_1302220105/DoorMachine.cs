dfb90bfb921ea89b7434bf3757ac949cede4114a
Mahesazain <echa.zain@gmail.com>
2024-03-12 23:49:57 +0800

    Menambahkan implementasi dengan state-based construction

--- tpmodul4_1302220105/DoorMachine.cs
+++ tpmodul4_1302220105/DoorMachine.cs
@@ -2,5 +2,32 @@
 {
     internal class DoorMachine
     {
+        private enum State
+        {
+            Terkunci,
+            Terbuka
+        }
+
+        private State _state;
+
+        public DoorMachine()
+        {
+            _state = State.Terkunci;
+            Console.WriteLine("Pintu terkunci");
+        }
+
+        public void ChangeState()
+        {
+            if (_state == State.Terkunci)
+            {
+                _state = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+            }
+            else
+            {
+                _state = State.Terkunci;
+                Console.WriteLine("Pintu terkunci");
+            }
+        }
     }
 }
--- tpmodul4_1302220105/Program.cs
+++ tpmodul4_1302220105/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace tpmodul4_1302220105
+﻿using tpmodul4_1302220105;
+
+namespace tpmodul4_1302220105
 {
     class Program
     {
@@ -6,13 +8,19 @@
         {
             for (int i = 0; i < 2; i++)
             {
-                Console.WriteLine("Soal B");
+                Console.WriteLine("Soal 3");
                 KodePos myKodepos = new KodePos();
                 Console.Write("Masukkan nama kelurahan untuk mendapatkan kode pos: ");
                 string kelurahan = Console.ReadLine();
                 string KodePos = myKodepos.GetKodepos(kelurahan);
                 Console.WriteLine($"Kode pos untuk '{kelurahan}' adalah: {KodePos}");
             }
+
+            Console.WriteLine("Soal 5");
+            DoorMachine doorMachine = new DoorMachine();
+            // Simulasi perubahan state
+            doorMachine.ChangeState(); // Pintu tidak terkunci
+            doorMachine.ChangeState(); // Pintu terkunci
         }
     }
 }
